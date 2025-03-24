3400823d0bd0fc4e56d85a2802a7e04958cc8a8a
Daxexp <exp12mt@gmail.com>
2025-03-25 00:33:47 +0530

    Update ps.js

--- ps.js
+++ ps.js
@@ -5,12 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
         const password = prompt("Please enter the password to access the content:");
 
         // Define the correct password
-        const correctPassword = "dass99"; // Set your password here
+        const correctPassword = "your-password"; // Set your password here
 
         // Check if the entered password is correct
         if (password === correctPassword) {
             // Store the authenticated state in session storage
             sessionStorage.setItem('authenticated', 'true');
+            // Show a success message
+            alert("Welcome..! :)");
         } else {
             // If the password is incorrect, display an error message and hide the content
             alert("Incorrect password. You do not have access to this content.");
