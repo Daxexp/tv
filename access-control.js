6f9f719b0aedf203e533c80dd6386707d0059bdc
Daxexp <exp12mt@gmail.com>
2025-04-11 00:18:46 +0530

    Update access-control.js

--- access-control.js
+++ access-control.js
@@ -6,8 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
     if (!document.referrer || !document.referrer.startsWith(authorizedReferrer)) {
         // If the referrer is invalid, check for session-based authentication
         if (sessionStorage.getItem('access_granted') !== 'true') {
-            // Redirect or block access if no valid session is found
-            alert("Unauthorized access! You cannot directly access this page.");
             window.location.href = authorizedReferrer; // Redirect to homepage
             return;
         }
